7d0bd6c0e20981fdbabe8b7cdec2867e97db0e51
Nacho <nkeiniger@gmail.com>
2021-12-24 23:41:06 -0300

    warnings fixed

--- src/components/taskbar/Taskbar.js
+++ src/components/taskbar/Taskbar.js
@@ -2,11 +2,6 @@ import { useEffect, useState } from "react";
 import styles from "./Taskbar.module.css";
 import OpenedFilesContainer from "./OpenedFilesContainer";
 
-function useForceUpdate(){
-    const [value, setValue] = useState(0); // integer state
-    return () => setValue(value => value + 1); // update the state to force render
-}
-
 export default function Taskbar({files, toggle}) {
   const [date, setDate] = useState( new Date().toLocaleString("es-AR") );
 
@@ -20,7 +15,7 @@ export default function Taskbar({files, toggle}) {
     <>
       <hr />
       <div className={styles.taskbar}>
-        <button className={styles.start} onClick={useForceUpdate}> Start </button>
+        <button className={styles.start}> Start </button>
         <OpenedFilesContainer className={styles.openedFiles} files={files} toggle={toggle} useForceUpdate={useForceUpdate}/>
         <button className={styles.time}> {date} </button>
       </div>
